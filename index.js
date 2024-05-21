496e0585e1b1b1a47b3409d9734b25af22c38abc
sow1432 <sowmyashree14222@gmail.com>
2024-05-21 15:17:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("js is here")
+let value;
+document.getElementByid("btnl").onclick= function(){
+}
+console.log("button pressed")
+value = document.getElementById("inp").value
+console.log(value)
+document.getElementById("heading").innerHTML = value
+}
